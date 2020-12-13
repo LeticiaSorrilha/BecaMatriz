3f11a0bf39f0e9dae1bf75867a657acdd2c60b76
Leticia Sorrilha Souza <leticiasorrilhageo@gmail.com>
2020-12-12 21:06:00 -0300

    Commit Desenvolvimento MatrizQuadrada_Leticia

--- BecaMatriz/Program.cs
+++ BecaMatriz/Program.cs
@@ -13,6 +13,29 @@ namespace BecaMatriz
             // Cada Becario deverá criar um metódo para calcular a soma da diagonal principal de uma matriz quadrada 3 x 3 
             // O método deverá ter o nome do participante
             // exemplo: CalculaSomaDiagonalPrincipal_Ricardo();
+
+            int[,] array33 = new int[3,3] { { 1, 2, 3 } , { 4, 5, 6 } , { 7, 8, 9 } };
+            somarDiagonalPrincipal_Leticia(array33);
+            Console.ReadLine();
+        }
+
+        public static void somarDiagonalPrincipal_Leticia(int[,] array33)
+        {
+            
+            int soma = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine(" " + i + " " + j + " ---->" + array33[i, j]);
+                    if (i == j)
+                    {
+                        soma += array33[i, j];
+                    }
+                }
+                Console.WriteLine("---------------");
+            }
+            Console.WriteLine("TOTAL: " + soma);
         }
     }
 }
